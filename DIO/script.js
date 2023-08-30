a6398b1f1a12073ec13b9d558395e981fdb79f1c
SamaraParcero <samaraparceroti@gmail.com>
2023-08-30 13:53:25 -0300

    estudando variáveis e operadores em javascript

---
+++ DIO/script.js
@@ -0,0 +1,64 @@
+alert("Tudo certo, jovens?")
+
+var tOuF = false;
+console.log(typeof(tOuF));
+
+var numero = 1;
+console.log(number);
+
+var nome = "samara";
+console.log(nome);
+nome = "parcero";
+console.log(nome);
+
+let variavel = 2;
+console.log(variavel);
+
+const constante = "muda? não"
+console.log(constante);
+
+var escopoglobal = "global";
+console.log(escopoglobal)
+
+function escopolocal(){
+    let escopolocal = 'local';
+    console.log(escopolocal);
+}
+
+var atribuicao = 'samara';
+
+var comparacao = '0' == 0;
+
+var comparacaoidentica = '0' === '0';
+
+var adicao = 1+1;
+console.log(adicao);
+
+var subtracao = 1-1;
+console.log(subtracao);
+
+var multiplicacao = 2*3;
+
+var divisaoReal = 6/2;
+
+var divisaoInteira = 5%3;
+
+var potenciacao = 2**10;
+
+var maiorque = 5>2;
+console.log(maiorque);
+
+var menorque = 5<2;
+console.log(menorque);
+
+var maiorigual = 5>=2;
+console.log(maiorigual);
+
+var menorigual = 5<=2;
+console.log(menorigual);
+
+var e = true && false;
+
+var ou = true || false;
+
+var nao = !true;
