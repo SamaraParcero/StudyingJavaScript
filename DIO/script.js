a0c2e7680d0daa4b860a2e3bfe6e0ad2f98c5098
SamaraParcero <samaraparceroti@gmail.com>
2023-08-30 19:49:45 -0300

    aprendendo vetores e objetos javascript

--- DIO/script.js
+++ DIO/script.js
@@ -62,3 +62,40 @@ var e = true && false;
 var ou = true || false;
 
 var nao = !true;
+
+let array = ['string', 1, true, ['array']]
+console.log(array[0]);
+
+array.forEach(function (item, index){console.log(item,index)});
+
+array.push('novo item');
+console.log(array);
+
+array.pop();
+console.log(array);
+
+array.shift();
+console.log(array);
+
+array.unshift('novo item no inicio');
+console.log(array);
+
+console.log(array.indexOf[true]);
+
+array.splice(0,3);
+console.log(array);
+
+let novoarray= array.splice(0,3);
+console.log(novoarray);
+
+let object = { string: 'string', number: 1, boolean: true, array: ['array'], objectinterno:['object interno'] };
+console.log(object.number);
+
+var string = (object.string);
+console.log(string);
+
+var arrayinterno = object.array;
+console.log(arrayinterno);
+
+var { string, boolean, objectinterno} = object;
+console.log(string, boolean, objectinterno);
