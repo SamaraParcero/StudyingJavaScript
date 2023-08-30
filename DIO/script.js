2ed8a63810c8e39c720d40bf13670127d9b7d462
SamaraParcero <samaraparceroti@gmail.com>
2023-08-30 20:33:31 -0300

    estudando estruturas de repetição

--- DIO/script.js
+++ DIO/script.js
@@ -99,3 +99,72 @@ console.log(arrayinterno);
 
 var { string, boolean, objectinterno} = object;
 console.log(string, boolean, objectinterno);
+
+
+//------------------ESTRUTURAS CONDICIONAIS-------------------------------
+
+var jogador1 =0;
+var jogador2 = 0;
+var placar;
+
+jogador1 !== -1 && jogador2 !== -1? console.log("Os jogadores são válidos"): console.log("Jogadores inválidos");
+console.log(jogador1 + jogador2);
+if (jogador1> 0) {
+    console.log("Jogador 1 marcou ponto");
+} else if (jogador2> 0) {
+    console.log("Jogador2 marcou ponto");
+} else {
+    console.log("Ninguém marcou ponto");
+}
+
+
+switch (placar){
+    case placar= jogador1>jogador2: 
+    console.log("Jogador 1 ganhou!");
+    break;
+    case placar = jogador1<jogador2: 
+    console.log("Jogador 2 ganhou!");
+    break;
+    default:
+        console.log("Ninguém ganhou");
+}
+
+let array1 = ['valor1', 'valor2', 'valor3' , 'valor4' , 'valor5' ];
+let  object1= {propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3', propriedade4: 'valor4', propriedade5: 'valor5', } ;
+
+for(let indice = 0; indice< array1.length; indice++){
+    console.log(indice);
+}
+
+//com array
+for(let i in array){
+    console.log(i);
+}
+
+//com object
+for (i in objetc){
+    console.log(i);
+
+}
+
+//com array
+for (i of array) {
+    console.log(i);
+}
+
+for (i of object1.propriedade1){
+    console.log(i);
+}
+
+var a = 0;
+
+while (a<10){
+    a++;
+    console.log(a);
+
+}
+
+do {
+    a++;
+    console.log(a);
+} while (a<10)
